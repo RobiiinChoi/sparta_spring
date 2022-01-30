7ecec54bd918596c0bbf4f199b9f1077a12baabb
RobiiinChoi <robin.choi11@gmail.com>
2022-01-30 17:44:55 +0900

    Update
    Update

--- Week2/src/main/java/com/sparta/week2/Week2Application.java
+++ Week2/src/main/java/com/sparta/week2/Week2Application.java
@@ -2,6 +2,7 @@ package com.sparta.week2;
 
 import com.sparta.week2.domain.Course;
 import com.sparta.week2.domain.CourseRepository;
+import com.sparta.week2.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,22 +21,28 @@ public class Week2Application {
 
 
     @Bean
-    public CommandLineRunner demo(CourseRepository repository){
+    public CommandLineRunner demo(CourseRepository courserepository, CourseService courseService){
         return (args) -> {
-            repository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+            courserepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
 
-            // 데이터 전부 조회
-            List<Course> courseList = repository.findAll();
-            for (int i=0; i<courseList.size();i++){
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courserepository.findAll();
+            for(int i =0; i<courseList.size();i++){
                 Course course = courseList.get(i);
                 System.out.println(course.getId());
                 System.out.println(course.getTitle());
                 System.out.println(course.getTutor());
             }
-            // 아이디 조회 or Exception Handling
-            Course course = repository.findById(1L).orElseThrow(
-                    () -> new NullPointerException("해당 아이디가 존재하지 않습니다")
-           );
+
+            Course new_course = new Course("웹 개발의 봄, Spring", "임민영");
+            courseService.update(1L, new_course);
+            courseList = courserepository.findAll();
+            for(int i = 0; i<courseList.size();i++){
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
         };
     }
 }
