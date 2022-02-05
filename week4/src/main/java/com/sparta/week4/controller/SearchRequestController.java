27129d2c9086b2c6530c08b915639a898ab84ae5
RobiiinChoi <robin.choi11@gmail.com>
2022-02-06 02:41:58 +0900

    API 검색
    API 검색파트 구현 완료

--- week4/src/main/java/com/sparta/week4/controller/SearchRequestController.java
+++ week4/src/main/java/com/sparta/week4/controller/SearchRequestController.java
@@ -1,4 +1,23 @@
 package com.sparta.week4.controller;
 
+import com.sparta.week4.models.ItemDto;
+import com.sparta.week4.utils.NaverShopSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> execSearch(@RequestParam String query){
+        String result = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(result);
+    }
 }
